dbbe4fe16d3da115e8a898b51714bb77ab586e5b
mr-black-8 <boz.black@gmail.com>
2016-05-09 20:43:05 +1000

    Improved existing functions, swal files added

--- main.js
+++ main.js
@@ -1,35 +1,7 @@
-
-// $( ".square" ).on("click", function() {
-//   var $curSquare = $( this );
-//   if($curSquare.children().css("opacity") == 0){
-//     $curSquare.children().animate({
-//       opacity: "1"
-//     },400);
-//   } else {
-//     $curSquare.children().animate({
-//       opacity: "0"
-//     },400);
-//   }
-// });
-
+// Gameboard array (x,y) co-ords from top left match array index.
 var gameboard = [["","",""],["","",""],["","",""]]
 
-
-$( ".square" ).on( "click", function(){
-  // debugger;
-  var $curSquare = $( this );
-  var $curID = $curSquare.attr("id");
-  var i = $curID[0];
-  var j = $curID[1];
-
-  if( gameboard[i][j] === "" ) {
-    gameboard[i][j] = "1"
-    return insertAxe($curID);
-  } else {
-    return shakeIcon($curID);
-  }
-});
-
+//Animate Shield image to square based on id paramater given
 var insertShield = function (iD) {
   var $curID = $( "#" + iD )
   $curID.children().attr("src", "images/shieldtp.png");
@@ -39,15 +11,17 @@ var insertShield = function (iD) {
   return;
 };
 
-var insertAxe = function (iD) {
+//Animate Cross image to square based on id paramater given
+var insertCross = function (iD) {
   var $curID = $( "#" + iD )
-  $curID.children().attr("src", "images/axetp.png");
+  $curID.children().attr("src", "images/crosstp.png");
   $curID.children().addClass("animated bounceIn").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
     $(this).removeClass();
   });
   return;
 };
 
+//Shake animation based in id paramater given (call if user clicks on occupyed square)
 var shakeIcon = function (iD) {
   var $curID = $( "#" + iD )
   $curID.children().addClass("animated shake").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
@@ -56,17 +30,115 @@ var shakeIcon = function (iD) {
   return;
 };
 
+//Easy setting computer move - random generation of (x,y), Loops until empty square is selected, add 'o' at gameboard[x][y], calls insertShield, calls winCheck
 var computerMove = function () {
-  // debugger;
-  var i = Math.floor(Math.random() * 3)
-  var j = Math.floor(Math.random() * 3)
-  var curID = i.toString() + j.toString();
+  var x = Math.floor(Math.random() * 3)
+  var y = Math.floor(Math.random() * 3)
+  var curID = x.toString() + y.toString();
 
-  if( gameboard[i][j] === "" ) {
-    gameboard[i][j] = "2";
-    return insertShield(curID);
+  if( gameboard[x][y] === "" ) {
+    gameboard[x][y] = "o";
+    insertShield(curID);
   } else {
-    return computerMove();
+    computerMove();
+    return;
+  }
+
+  if(winCheck(x,y)) {
+    swal("Computer Wins!");
+    resetBoard();
+    return;
+  } else {
+    return;
   }
 
 };
+
+//Player move, called when square is clicked. id of 'this' square is converted to (x,y), add 'x' at gameboard[x][y], calls insertCross
+var playerMove = function () {
+  var $curSquare = $( this );
+  var $curID = $curSquare.attr("id");
+  var x = $curID[0];
+  var y = $curID[1];
+
+  if( gameboard[x][y] === "" ) {
+    gameboard[x][y] = "x"
+    insertCross($curID);
+  } else {
+    shakeIcon($curID);
+    return;
+  }
+
+  if(winCheck(x,y)) {
+    alert("Player Wins!");
+    // swal("Player Wins!");
+    resetBoard();
+  } else {
+    window.setTimeout( computerMove, 200 );
+  }
+
+};
+
+// check each line for win condition.
+var winCheck = function(x,y) {
+
+  var horString = ""
+  var vertString = ""
+  var diaString = ""
+  var revDiaString = ""
+
+  //Check [x][i] (horizontal line)
+  for( var i = 0; i < gameboard.length; i++ ) {
+    var j = gameboard[x][i];
+    horString += j;
+  }
+
+  //Check [i][y] (vertical line)
+  for( var i = 0; i < gameboard.length; i++ ) {
+    var j = gameboard[i][y]
+    vertString += j;
+  }
+
+  //Check [i][i] (diagonal line)
+  for( var i = 0; i < gameboard.length; i++ ) {
+    var j = gameboard[i][i];
+    diaString += j;
+  }
+
+  //Check [i][.length - 1 - i] (rev. diagonal line)
+  for( var i = 0; i < gameboard.length; i++ ) {
+    var n = gameboard.length;
+    var j = gameboard[i][n - 1 - i];
+    revDiaString += j;
+  }
+
+  if( (horString === "xxx" || vertString === "xxx" ) || (horString === "ooo" || vertString === "ooo" ) ) {
+    return true;
+  } else if( (diaString === "xxx" || revDiaString === "xxx" ) || (diaString === "ooo" || revDiaString === "ooo" ) ){
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//Clear gameboard array and img src from .square divs.
+var resetBoard = function() {
+
+  // set all values in gameboard array to ""
+  for( var i = 0; i < gameboard.length; i++ ){
+    for( var j = 0; j < gameboard[i].length; j ++ ) {
+      gameboard[i][j] = "";
+    }
+  }
+
+  //set all src attrbutes to ""
+  $( ".square" ).each( function(i) {
+    $( this ).children().attr("src", "");
+    // $( this ).children().addClass("animated fadeOutUp").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+    //   $( this ).removeClass();
+    //   $( this ).children().attr("src", "");
+    // });
+  });
+};
+
+$( ".square" ).on( "click", playerMove );
