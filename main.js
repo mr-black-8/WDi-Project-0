9226d4d2a3d0bfdb2ee4f7e0cdeb3e4ea77a24c9
mr-black-8 <boz.black@gmail.com>
2016-05-09 12:30:22 +1000

    Player & Computer move functions complete

--- main.js
+++ main.js
@@ -12,18 +12,61 @@
 //   }
 // });
 
-var toggleIcon = function () {
-  var $curSquare = $( this );
+var gameboard = [["","",""],["","",""],["","",""]]
 
-  if($curSquare.children().attr("class") === undefined || $curSquare.children().attr("class") === ""){
-    $curSquare.children().addClass("animated bounceIn").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-      $(this).removeClass();
-    });
+
+$( ".square" ).on( "click", function(){
+  // debugger;
+  var $curSquare = $( this );
+  var $curID = $curSquare.attr("id");
+  var i = $curID[0];
+  var j = $curID[1];
+
+  if( gameboard[i][j] === "" ) {
+    gameboard[i][j] = "1"
+    return insertAxe($curID);
   } else {
-    $curSquare.children().addClass("animated bounceOut").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-      $(this).removeClass();
-    });
+    return shakeIcon($curID);
   }
+});
+
+var insertShield = function (iD) {
+  var $curID = $( "#" + iD )
+  $curID.children().attr("src", "images/shieldtp.png");
+  $curID.children().addClass("animated bounceIn").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+    $(this).removeClass();
+  });
+  return;
 };
 
-$( ".square" ).on( "click", toggleIcon );
+var insertAxe = function (iD) {
+  var $curID = $( "#" + iD )
+  $curID.children().attr("src", "images/axetp.png");
+  $curID.children().addClass("animated bounceIn").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+    $(this).removeClass();
+  });
+  return;
+};
+
+var shakeIcon = function (iD) {
+  var $curID = $( "#" + iD )
+  $curID.children().addClass("animated shake").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+    $(this).removeClass();
+  });
+  return;
+};
+
+var computerMove = function () {
+  // debugger;
+  var i = Math.floor(Math.random() * 3)
+  var j = Math.floor(Math.random() * 3)
+  var curID = i.toString() + j.toString();
+
+  if( gameboard[i][j] === "" ) {
+    gameboard[i][j] = "2";
+    return insertShield(curID);
+  } else {
+    return computerMove();
+  }
+
+};
